52d410fda4da95ec484f2dee3fcaa938d3110046
Jaeeon.Bae <baejae89@naver.com>
2021-09-06 18:05:26 +0900

    [Android] Base Architecture

--- app/src/main/java/com/eidem/integratedmovielookup/MainActivity.kt
+++ app/src/main/java/com/eidem/integratedmovielookup/MainActivity.kt
@@ -2,10 +2,15 @@ package com.eidem.integratedmovielookup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.eidem.integratedmovielookup.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
     }
 }
---
+++ app/src/main/java/com/eidem/integratedmovielookup/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.eidem.integratedmovielookup
+
+import android.app.Activity
+import com.eidem.integratedmovielookup.databinding.ActivityMainBinding
+
+class MainViewModel(activity: Activity, binding: ActivityMainBinding) {
+
+}
