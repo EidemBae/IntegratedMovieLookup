dd995f1b0b5cb74bc1432afb44b3c9a687e49955
Jaeeon.Bae <baejae89@naver.com>
2021-09-06 20:50:52 +0900

    [Android] 영화 포스터 출력 목록

--- app/src/main/java/com/eidem/integratedmovielookup/MainActivity.kt
+++ app/src/main/java/com/eidem/integratedmovielookup/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.eidem.integratedmovielookup
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.eidem.integratedmovielookup.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        initView()
+    }
 
+    override fun initView() {
+        viewModel = MainViewModel(this, binding)
+        viewModel.requestBoxOfficeList()
     }
 }
--- app/src/main/java/com/eidem/integratedmovielookup/MainViewModel.kt
+++ app/src/main/java/com/eidem/integratedmovielookup/MainViewModel.kt
@@ -1,8 +1,60 @@
 package com.eidem.integratedmovielookup
 
 import android.app.Activity
+import com.bumptech.glide.Glide
+import com.eidem.integratedmovielookup.adapter.BoxOfficeAdapter
+import com.eidem.integratedmovielookup.api.ApiUtil
+import com.eidem.integratedmovielookup.constants.ApiConstant
+import com.eidem.integratedmovielookup.constants.AppConstant
 import com.eidem.integratedmovielookup.databinding.ActivityMainBinding
+import com.eidem.integratedmovielookup.model.BoxOfficeModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class MainViewModel(activity: Activity, binding: ActivityMainBinding) {
+class MainViewModel(activity: Activity, val binding: ActivityMainBinding) {
+    private var boxOfficeModelList: List<BoxOfficeModel>? = null
 
+    init {
+
+    }
+
+    private fun getBoxOfficePosterUrl(url: String): String {
+        return url.replace("/thumb/","/")
+            .replace("/thn_","/")
+    }
+
+    fun requestBoxOfficeList() {
+        ApiUtil.getKobisApiService()
+            .requestBoxOfficeList()
+            .enqueue(object: Callback<List<BoxOfficeModel>> {
+                override fun onResponse(
+                    call: Call<List<BoxOfficeModel>>,
+                    response: Response<List<BoxOfficeModel>>
+                ) {
+                    boxOfficeModelList = response.body()
+
+                    boxOfficeModelList?.let {
+                        val url = AppConstant.NETWORK_HTTPS + ApiConstant.KOBIS + getBoxOfficePosterUrl(it[0].thumbUrl)
+                        println("===> $url")
+//                        Glide.with(binding.image).load(url).into(binding.image)
+                        setBoxOffice(it)
+                        for (i in it.indices) {
+                            println("====> ${it[i].movieNm}")
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<List<BoxOfficeModel>>, t: Throwable) {
+                }
+            })
+    }
+
+    private fun setBoxOffice(itemList: List<BoxOfficeModel>) {
+        println("======> BoxOffice ${itemList.size}")
+        binding.recyclerView.adapter = BoxOfficeAdapter(itemList)
+        binding.recyclerView.set3DItem(true)
+        binding.recyclerView.setAlpha(true)
+
+    }
 }
